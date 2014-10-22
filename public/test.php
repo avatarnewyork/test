5a1cbe4620a3ecab7f90b6ffb1d70cd9fb371fe6
patrick tully <patrick@avatarnewyork.com>
2014-10-22 14:28:00 -0400

    added test

---
+++ public/test.php
@@ -0,0 +1,13 @@
+<? 
+
+print "Port: " . getenv("DB_1_PORT_3306_TCP_PORT") ."\n";
+print "Host: " . getenv("DB_1_PORT_3306_TCP_ADDR") ."\n";
+
+//print $argv[1] . "\n"
+
+//if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') { echo "HTTPS"; }
+//print_r($_SERVER) 
+//print_r(getallheaders());
+
+
+ ?>
